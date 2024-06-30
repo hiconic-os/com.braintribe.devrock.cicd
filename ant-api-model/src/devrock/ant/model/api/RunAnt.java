895656a719f91a235cc1f0958eee759401f92c73
Peter Gazdik <peter.gazdik@gmail.com>
2024-06-30 15:47:15 +0200

    Adding RunAnt.buildFile

--- ant-api-model/src/devrock/ant/model/api/RunAnt.java
+++ ant-api-model/src/devrock/ant/model/api/RunAnt.java
@@ -4,6 +4,8 @@ import java.util.Map;
 
 import com.braintribe.model.generic.annotation.Initializer;
 import com.braintribe.model.generic.annotation.meta.Alias;
+import com.braintribe.model.generic.annotation.meta.FileName;
+import com.braintribe.model.generic.annotation.meta.FolderName;
 import com.braintribe.model.generic.annotation.meta.PositionalArguments;
 import com.braintribe.model.generic.annotation.meta.UnsatisfiedBy;
 import com.braintribe.model.generic.eval.EvalContext;
@@ -19,32 +21,41 @@ import devrock.ant.model.reason.AntBuildFailed;
 @Alias("ant")
 @PositionalArguments("target")
 public interface RunAnt extends AntRequest {
+
 	EntityType<RunAnt> T = EntityTypes.T(RunAnt.class);
-	
+
 	String projectDir = "projectDir";
 	String target = "target";
 	String properties = "properties";
 	String bufferOutput = "bufferOutput";
 	String ownerInfo = "ownerInfo";
-	
+
 	@Alias("p")
+	@FolderName
 	@Initializer("'.'")
 	String getProjectDir();
 	void setProjectDir(String projectDir);
-	
+
 	@Alias("t")
 	String getTarget();
 	void setTarget(String target);
-	
+
+	@Alias("f")
+	@FileName
+	@Initializer("'build.xml'")
+	String getBuildFile();
+	void setBuildFile(String buildFile);
+
 	String getOwnerInfo();
 	void setOwnerInfo(String ownerInfo);
-	
+
 	Map<String, String> getProperties();
 	void setProperties(Map<String, String> properties);
-	
+
 	boolean getBufferOutput();
 	void setBufferOutput(boolean bufferOutput);
-	
+
 	@Override
 	EvalContext<Neutral> eval(Evaluator<ServiceRequest> evaluator);
+
 }
--- ant-rx-module/src/com/braintribe/devrock/cicd/ant/processing/AntProcessor.java
+++ ant-rx-module/src/com/braintribe/devrock/cicd/ant/processing/AntProcessor.java
@@ -31,7 +31,6 @@ import com.braintribe.console.output.ConsoleOutput;
 import com.braintribe.gm.model.reason.Maybe;
 import com.braintribe.gm.model.reason.Reasons;
 import com.braintribe.gm.model.reason.essential.InvalidArgument;
-import com.braintribe.model.processing.service.api.ServiceRequestContext;
 import com.braintribe.model.processing.service.impl.AbstractDispatchingServiceProcessor;
 import com.braintribe.model.processing.service.impl.DispatchConfiguration;
 import com.braintribe.model.service.api.result.Neutral;
@@ -44,60 +43,55 @@ import devrock.ant.model.api.AntRequest;
 import devrock.ant.model.api.RunAnt;
 import devrock.ant.model.reason.AntBuildFailed;
 
-public class AntProcessor extends AbstractDispatchingServiceProcessor<AntRequest, Object>{
-	
+public class AntProcessor extends AbstractDispatchingServiceProcessor<AntRequest, Object> {
+
 	private static Lock bufferingLock = new ReentrantLock();
 	private StreamPipeFactory streamPipeFactory;
 	private File antLibDir;
-	
+
 	@Configurable
 	public void setAntLibDir(File antLibDir) {
 		this.antLibDir = antLibDir;
 	}
-	
+
 	@Required
 	public void setStreamPipeFactory(StreamPipeFactory streamPipeFactory) {
 		this.streamPipeFactory = streamPipeFactory;
 	}
-	
+
 	@Override
 	protected void configureDispatching(DispatchConfiguration<AntRequest, Object> dispatching) {
-		dispatching.registerReasoned(RunAnt.T, this::runAnt);
-		
+		dispatching.registerReasoned(RunAnt.T, (c, r) -> runAnt(r));
 	}
-	
+
 	interface Outputs extends Closeable {
 		PrintStream out();
 		PrintStream err();
-		
+
 		void notifyBuildException(BuildException e);
 		void notifyDuration(long ms);
-		
+
 		@Override
 		default void close() throws IOException {
+			/* NOOP */
 		}
 	}
-	
+
 	class DirectOutputs implements Outputs {
-		
-		private RunAnt request;
-		private File projectDir;
+
+		private final RunAnt request;
+		private final File projectDir;
 
 		public DirectOutputs(RunAnt request, File projectDir) {
 			this.request = request;
 			this.projectDir = projectDir;
 		}
-		
-		@Override
-		public PrintStream err() {
-			return System.err;
-		}
-		
-		@Override
-		public PrintStream out() {
-			return System.out;
-		}
-		
+
+		// @formatter:off
+		@Override public PrintStream err() { return System.err; }
+		@Override public PrintStream out() { return System.out; }
+		// @formatter:on
+
 		@Override
 		public void notifyDuration(long ms) {
 			String ownerInfo = request.getOwnerInfo();
@@ -106,82 +100,68 @@ public class AntProcessor extends AbstractDispatchingServiceProcessor<AntRequest
 
 			println(duration(ms, ownerInfo));
 		}
-		
+
 		@Override
 		public void notifyBuildException(BuildException e) {
 			println(brightRed(errorIfRelevant(e)));
 		}
 	}
-	
+
 	class BufferedOutputs implements Outputs {
-		private StreamPipe pipe;
-		private PrintStream ps;
-		private RunAnt request;
-		private File projectDir;
+		private final StreamPipe pipe;
+		private final PrintStream ps;
+		private final RunAnt request;
+		private final File projectDir;
 		private BuildException buildException;
 		private long duration;
-		
+
 		public BufferedOutputs(RunAnt request, File projectDir) {
 			this.request = request;
 			this.projectDir = projectDir;
 			pipe = streamPipeFactory.newPipe("ant-output-buffer");
-			
+
 			Charset charset = System.out.charset();
-			
+
 			ps = new PrintStream(pipe.openOutputStream(), false, charset);
 		}
-		
-		@Override
-		public PrintStream err() {
-			return ps;
-		}
-		
-		@Override
-		public PrintStream out() {
-			return ps;
-		}
-		
-		@Override
-		public void notifyDuration(long ms) {
-			this.duration = ms;
-		}
-		
-		@Override
-		public void notifyBuildException(BuildException e) {
-			buildException = e; 
-		}
-		
+
+		// @formatter:off
+		@Override public PrintStream err() { return ps; }
+		@Override public PrintStream out() { return ps; }
+		@Override public void notifyDuration(long ms) { this.duration = ms; }
+		@Override public void notifyBuildException(BuildException e) { buildException = e; }
+		// @formatter:on
+
 		@Override
 		public void close() throws IOException {
 			ps.close();
-			
+
 			String ownerInfo = request.getOwnerInfo();
 			if (ownerInfo == null)
 				ownerInfo = projectDir.getName();
 
 			bufferingLock.lock();
-			
+
 			try {
 				println(sequence( //
 						text("===================================================\n"), //
 						white("Executing ant target "), brightYellow(request.getTarget()), text(" for "), cyan(ownerInfo), text("\n")));
-				
+
 				try (InputStream in = pipe.openInputStream()) {
 					IOTools.transferBytes(in, System.out);
 				}
-				
+
 				println(sequence( //
 						brightRed(errorIfRelevant(buildException)), //
 						duration(duration, ownerInfo), //
 						text("\n") //
 				));
-			}
-			finally {
+			} finally {
 				bufferingLock.unlock();
 			}
 		}
 	}
-	
+
 	private ConsoleOutput duration(long durationMs, String folderName) {
 		if (durationMs < 0)
 			return text("");
@@ -189,7 +169,7 @@ public class AntProcessor extends AbstractDispatchingServiceProcessor<AntRequest
 		String duration = StringTools.prettyPrintMilliseconds(durationMs, true);
 		return sequence(text("Building "), cyan(folderName), text(" took "), yellow(duration));
 	}
-	
+
 	/* This is here, because in some cases Ant doesn't log the error, but only throws a BuildException (e.g. when instantiating a POJO in build.xml
 	 * using an invalid property name.) */
 	private ConsoleOutput errorIfRelevant(BuildException e) {
@@ -205,78 +185,79 @@ public class AntProcessor extends AbstractDispatchingServiceProcessor<AntRequest
 		);
 	}
 
-	
 	private Outputs openOutputs(RunAnt request, File projectDir) {
 		if (!request.getBufferOutput())
 			return new DirectOutputs(request, projectDir);
-		
+
 		return new BufferedOutputs(request, projectDir);
 	}
-	
-	public Maybe<Neutral> runAnt(ServiceRequestContext requestContext, RunAnt request) {
-        
-        String p = request.getProjectDir();
-        if (p == null)
-        	p = ".";
-		
-        File projectDir = new File(p);
-        
-        try (Outputs outputs = openOutputs(request, projectDir)) {
-            Project project = new Project();
-            
-            DefaultLogger logger = new DefaultLogger();
-            logger.setErrorPrintStream(outputs.err());
-            logger.setOutputPrintStream(outputs.out());
-            logger.setMessageOutputLevel(Project.MSG_INFO);
-            
-	        project.addBuildListener(logger);
-	        project.init();
-	        
+
+	private Maybe<Neutral> runAnt(RunAnt request) {
+		String buildXmlFile = request.getBuildFile();
+		if (buildXmlFile == null)
+			buildXmlFile = "build.xml";
+
+		String p = request.getProjectDir();
+		if (p == null)
+			p = ".";
+
+		File projectDir = new File(p);
+
+		try (Outputs outputs = openOutputs(request, projectDir)) {
+			Project project = new Project();
+
+			DefaultLogger logger = new DefaultLogger();
+			logger.setErrorPrintStream(outputs.err());
+			logger.setOutputPrintStream(outputs.out());
+			logger.setMessageOutputLevel(Project.MSG_INFO);
+
+			project.addBuildListener(logger);
+			project.init();
+
 			project.setBaseDir(projectDir);
-	
-	        ProjectHelper projectHelper = ProjectHelper.getProjectHelper();
-	        project.addReference("ant.projectHelper", projectHelper);
-	        
-	        if (antLibDir != null)
-	        	project.setProperty("ant.library.dir", antLibDir.getAbsolutePath());
-	
-	        // transfer properties
-	        for (Map.Entry<String, String> entry: request.getProperties().entrySet()) {
-	        	project.setProperty(entry.getKey(), entry.getValue());
-	        }
-	        
-	        long start = System.currentTimeMillis();
 
-	        Demuxing.bindSubProjectToCurrentThread(project);
-	        
-	        try {
-	        	projectHelper.parse(project, new File(projectDir, "build.xml"));
-	        	
-	        	String target = request.getTarget();
-	        	
-	        	if (target == null)
+			ProjectHelper projectHelper = ProjectHelper.getProjectHelper();
+			project.addReference("ant.projectHelper", projectHelper);
+
+			if (antLibDir != null)
+				project.setProperty("ant.library.dir", antLibDir.getAbsolutePath());
+
+			// transfer properties
+			for (Map.Entry<String, String> entry : request.getProperties().entrySet()) {
+				project.setProperty(entry.getKey(), entry.getValue());
+			}
+
+			long start = System.currentTimeMillis();
+
+			Demuxing.bindSubProjectToCurrentThread(project);
+
+			try {
+				projectHelper.parse(project, new File(projectDir, buildXmlFile));
+
+				String target = request.getTarget();
+
+				if (target == null)
 					target = project.getDefaultTarget();
-	        	
-	        	if (target == null)
-	        		return Reasons.build(InvalidArgument.T).text("RunAnt.target must not be null for the project " + projectDir.getName() + " as it has no default target").toMaybe();
-	        	
-	        	project.executeTarget(target);
-	        }
-	        catch (BuildException e) {
-	        	outputs.notifyBuildException(e);
-	        	return Reasons.build(AntBuildFailed.T).text(e.getMessage()).toMaybe();
-	        }
-	        finally {
-	        	Demuxing.unbindSubProjectFromCurrentThread();
-	        	
-	        	long duration = System.currentTimeMillis() - start;
-	        	outputs.notifyDuration(duration);
-	        }
-        }
-        catch (IOException e) {
-        	throw new UncheckedIOException(e);
-        }
 
-        return Maybe.complete(Neutral.NEUTRAL);
+				if (target == null)
+					return Reasons.build(InvalidArgument.T)
+							.text("RunAnt.target must not be null for the project " + projectDir.getName() + " as it has no default target")
+							.toMaybe();
+
+				project.executeTarget(target);
+			} catch (BuildException e) {
+				outputs.notifyBuildException(e);
+				return Reasons.build(AntBuildFailed.T).text(e.getMessage()).toMaybe();
+			} finally {
+				Demuxing.unbindSubProjectFromCurrentThread();
+
+				long duration = System.currentTimeMillis() - start;
+				outputs.notifyDuration(duration);
+			}
+		} catch (IOException e) {
+			throw new UncheckedIOException(e);
+		}
+
+		return Maybe.complete(Neutral.NEUTRAL);
 	}
 }
