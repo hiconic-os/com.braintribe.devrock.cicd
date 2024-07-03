77e1e85378905bb4735bdfef7f6fefebf226b4f7
Peter Gazdik <peter.gazdik@gmail.com>
2024-07-03 10:27:49 +0200

    Fixing Apache License

--- devrock-cicd-steps/src/devrock/process/execution/ProcessResults.java
+++ devrock-cicd-steps/src/devrock/process/execution/ProcessResults.java
@@ -1,4 +1,6 @@
 // ============================================================================
+// Copyright BRAINTRIBE TECHNOLOGY GMBH, Austria, 2002-2022
+//
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
@@ -11,14 +13,6 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 // ============================================================================
-// ============================================================================
-// BRAINTRIBE TECHNOLOGY GMBH - www.braintribe.com
-// Copyright BRAINTRIBE TECHNOLOGY GMBH, Austria, 2002-2018 - All Rights Reserved
-// It is strictly forbidden to copy, modify, distribute or use this code without written permission
-// To this file the Braintribe License Agreement applies.
-// ============================================================================
-
-
 package devrock.process.execution;
 
 
