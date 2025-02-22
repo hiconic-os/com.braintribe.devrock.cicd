414a1803dedeaf444a7dd4e2c20459f604ee93ce
Peter Gazdik <peter.gazdik@gmail.com>
2025-02-22 19:55:59 +0100

    Generifying GM Enum BuildReason

--- devrock-cicd-api-model/src/devrock/cicd/model/api/data/BuildReason.java
+++ devrock-cicd-api-model/src/devrock/cicd/model/api/data/BuildReason.java
@@ -17,13 +17,13 @@ import com.braintribe.model.generic.base.EnumBase;
 import com.braintribe.model.generic.reflection.EnumType;
 import com.braintribe.model.generic.reflection.EnumTypes;
 
-public enum BuildReason implements EnumBase {
+public enum BuildReason implements EnumBase<BuildReason> {
 	NONE, UNPUBLISHED, ARTIFACT_CHANGED, DEPENDENCY_RESOLUTION_CHANGED, ARTIFACT_UNTRACKED, PARENT_CHANGED, EXPLICIT;
 	
-	public static EnumType T = EnumTypes.T(BuildReason.class);
+	public static EnumType<BuildReason> T = EnumTypes.T(BuildReason.class);
 
 	@Override
-	public EnumType type() {
+	public EnumType<BuildReason> type() {
 		return T;
 	}
 }
